1582c6a2fc0e01a4d4d4dc9b9f587a900054b27f
王渝 <wwangyuu@outlook.com>
2022-05-03 16:33:08 +0800

    更新

---
+++ src/Mpv.NET/API/Structs/MpvOpenglInitParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mpv.NET.API.Structs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MpvOpenglInitParams
+    {
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        GetProcAddressFn GetProcAddress;
+
+        IntPtr GetProcAddressCtx;
+    }
+
+    public delegate void GetProcAddressFn(IntPtr ctx, string name);
+
+}
